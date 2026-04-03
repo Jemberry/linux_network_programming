e70195b3b26334a96a3f19d508a47c5c48d16305
Jemberry <2425770361@qq.com>
2026-04-03 20:59:15 +0800

    Add files via upload

---
+++ LinuxDay05/11_pthread_cond_wait.c
@@ -0,0 +1,66 @@
+#include <my_header.h>
+
+pthread_mutex_t lock;
+pthread_cond_t cond;
+
+void *thread_func(void *arg)
+{
+    sleep(1);
+    //子线程再上
+    int ret = pthread_mutex_lock(&lock);
+    THREAD_ERROR_CHECK(ret, "pthread_mutex_lock");
+
+    //访问的共享资源
+    printf("I am son\n");
+
+    //先让子线程解锁
+    ret = pthread_mutex_unlock(&lock);
+    THREAD_ERROR_CHECK(ret, "son pthread_mutex_unlock1");
+
+    pthread_exit((void *)NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    //初始化互斥锁
+    int ret = pthread_mutex_init(&lock, NULL);
+    THREAD_ERROR_CHECK(ret, "pthread_mutex_init");
+
+    //初始化条件变量
+    ret = pthread_cond_init(&cond, NULL);
+    THREAD_ERROR_CHECK(ret, "pthread_cond_init");
+
+    //创建子线程
+    pthread_t thread_id;
+    ret = pthread_create(&thread_id, NULL, thread_func, NULL);//创建了子线程
+    THREAD_ERROR_CHECK(ret, "pthread_create");
+
+    ret = pthread_mutex_lock(&lock);
+    THREAD_ERROR_CHECK(ret, "main pthread_mutex_lock1");
+
+    printf("begin wait\n");
+    //因为没有满足条件，而睡眠
+    ret = pthread_cond_wait(&cond, &lock);
+    THREAD_ERROR_CHECK(ret, "main pthread_cond_wait");
+
+    printf("I am main condition ok\n");
+
+    //解锁
+    ret = pthread_mutex_unlock(&lock);
+    THREAD_ERROR_CHECK(ret, "main pthread_mutex_unlock1");
+
+    //主线程等子线程
+    ret = pthread_join(thread_id, NULL);
+    THREAD_ERROR_CHECK(ret, "pthread_join");
+
+    //最后一步，需要回收锁的资源
+    ret = pthread_mutex_destroy(&lock);
+    THREAD_ERROR_CHECK(ret, "pthread_mutex_destroy1");
+
+    //条件变量的销毁
+    ret = pthread_cond_destroy(&cond);
+    THREAD_ERROR_CHECK(ret, "pthread_cond_destroy");
+
+    return 0;
+}
+
